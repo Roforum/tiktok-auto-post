3f08adb502e1d227c6bb821e1db21c2049b30dd2
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2021-12-01 13:56:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import praw
 import requests
 import os
 import atexit
-from time import *
-from moviepy.editor import *
+import json
 import undetected_chromedriver as uc
+from time import sleep
+from moviepy.editor import *
 from fake_useragent import UserAgent, FakeUserAgentError
+from selenium.webdriver.common.by import By
+
+config = json.load(open('config.json'))
 
 options = uc.ChromeOptions()
 #options.headless=True
@@ -19,19 +23,27 @@ driver.maximize_window()
 driver.get("https://www.tiktok.com/login")
 
 reddit = praw.Reddit(
-    client_id="",
-    client_secret="",
-    password="",
-    user_agent="script by u/",
-    username="",
+    client_id=config["reddit_client_id"],
+    client_secret=config["reddit_client_secret"],
+    password=config["reddit_password"],
+    user_agent="script by u/" + config["reddit_username"],
+    username=config["reddit_username"],
 )
 reddit.read_only = True
 
 def getImage():
-    for submission in reddit.subreddit("memes").random_rising(limit=25):
+    for submission in reddit.subreddit(str(config["subreddits"])).random_rising(limit=25):
         url = submission.url
         if url.endswith(('.jpg', '.png', '.jpeg')):
             return url
+
+def makeImage():
+    try:
+        r = requests.get(getImage())
+        with open("assets/image.png", "wb") as f:
+            f.write(r.content)
+    except:
+        makeImage()
 
 def deleteFiles():
     os.remove("assets/image.png")
@@ -49,21 +61,21 @@ while True:
     
     video = VideoFileClip("assets/background.mp4")
 
-    r = requests.get(getImage())
-    with open("assets/image.png", "wb") as f:
-        f.write(r.content)
+    makeImage()
     
     image = ImageClip("assets/image.png").set_position(("center", "center")).resize(height = (video.h / 2), width = (video.w / 2))
     
     result = CompositeVideoClip([video, image]).set_duration(video.duration)
     result.write_videofile("assets/finalVideo.mp4", fps = 60, verbose = False, logger = None)
 
-    inputBox = driver.find_element_by_xpath("//input[@type='file']")
-    inputBox.send_keys("assets/finalVideo.mp4")
+    sleep(3)
+    
+    inputBox = driver.find_element(By.NAME, "upload-btn")
+    inputBox.send_keys(os.getcwd() + "\\assets\\finalVideo.mp4")
 
     sleep(3)
 
     print("Removing image and video")
-    deleteFiles()
+    #deleteFiles()
 
-    sleep(1)
+    sleep(10000)
